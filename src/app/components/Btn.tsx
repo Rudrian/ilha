c544f29b474f74d5173150e61693ebcdd676100d
Rudrian <162451660+Rudrian@users.noreply.github.com>
2025-09-16 13:54:08 -0300

    Update Btn.tsx

--- src/app/components/Btn.tsx
+++ src/app/components/Btn.tsx
@@ -1,13 +1,14 @@
 import { ArrowUpRight } from "lucide-react";
 import "./Btn.css";
-
-export default function Btn(props: { name: string }) {
-  return (
-    <button className="adventure-button">
-      {props.name}
-      <span className="icon-circle">
-        <ArrowUpRight color="white" strokeWidth={2.5} size={24} />
-      </span>
-    </button>
-  );
+ 
+export default function Btn(props:{name:string}) {
+    return (
+        <button>
+            <span className="adventure-button">{props.name}</span>
+            <div className="meio"></div>
+            <div className="icon-circle">
+            <ArrowUpRight color="white" strokeWidth={2.5} size={24} />
+            </div>
+        </button>
+    );
 }
